a00f21a3147fd55e7ae382a6507c4649bc47d479
Matias Paavilainen <paavilainen.matias@gmail.com>
2018-08-25 14:17:30 +0300

    Initial commit

---
+++ src/main/java/fi/matiaspaavilainen/masuitewarps/MaSuiteWarps.java
@@ -0,0 +1,11 @@
+package fi.matiaspaavilainen.masuitewarps;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class MaSuiteWarps extends Plugin {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+}
