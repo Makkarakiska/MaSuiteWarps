60dc368a7d8e7db3394b6b524d8a39af0ba16255
Masa <masa@simasasolutions.com>
2018-08-29 21:51:29 +0300

    Almost finished, getting pitch still bugging

--- src/main/java/fi/matiaspaavilainen/masuitewarps/commands/Teleport.java
+++ src/main/java/fi/matiaspaavilainen/masuitewarps/commands/Teleport.java
@@ -4,8 +4,13 @@ import fi.matiaspaavilainen.masuitecore.chat.Formator;
 import fi.matiaspaavilainen.masuitecore.config.Configuration;
 import fi.matiaspaavilainen.masuitewarps.Warp;
 import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 
 public class Teleport extends Command {
     public Teleport() {
@@ -26,6 +31,25 @@ public class Teleport extends Command {
             if(warp.getServer() == null){
                 formator.sendMessage(p, config.load("warps", "messages.yml").getString("warp-not-found"));
             }else{
+                ByteArrayOutputStream b = new ByteArrayOutputStream();
+                DataOutputStream out = new DataOutputStream(b);
+                try {
+                    if (!p.getServer().getInfo().getName().equals(warp.getServer())) {
+                        p.connect(ProxyServer.getInstance().getServerInfo(warp.getName()));
+                    }
+                    out.writeUTF("WarpPlayer");
+                    out.writeUTF(String.valueOf(p.getUniqueId()));
+                    out.writeUTF(warp.getWorld());
+                    out.writeDouble(warp.getX());
+                    out.writeDouble(warp.getY());
+                    out.writeDouble(warp.getZ());
+                    out.writeFloat(warp.getYaw());
+                    out.writeFloat(warp.getPitch());
+                    p.getServer().sendData("BungeeCord", b.toByteArray());
+
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
                 formator.sendMessage(p, config.load("warps", "messages.yml").getString("teleported").replace("%warp%", warp.getName()));
             }
 
