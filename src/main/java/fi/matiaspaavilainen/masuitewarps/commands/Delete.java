88f65f2d6716a7546af1b5874428ec6c689054a5
Masa <masa@simasasolutions.com>
2018-12-25 16:51:35 +0200

    Added warp removal behind check

--- src/main/java/fi/matiaspaavilainen/masuitewarps/commands/Delete.java
+++ src/main/java/fi/matiaspaavilainen/masuitewarps/commands/Delete.java
@@ -1,11 +1,17 @@
 package fi.matiaspaavilainen.masuitewarps.commands;
 
+import fi.matiaspaavilainen.masuitecore.Debugger;
 import fi.matiaspaavilainen.masuitecore.chat.Formator;
 import fi.matiaspaavilainen.masuitecore.config.Configuration;
 import fi.matiaspaavilainen.masuitewarps.Warp;
-import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
-import net.md_5.bungee.api.plugin.Command;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.Map;
 
 public class Delete {
 
@@ -20,6 +26,22 @@ public class Delete {
         }
         if (warp.delete(name)) {
             formator.sendMessage(p, config.load("warps", "messages.yml").getString("warp-deleted"));
+            try (ByteArrayOutputStream b = new ByteArrayOutputStream();
+                 DataOutputStream out = new DataOutputStream(b)) {
+                out.writeUTF("DelWarp");
+                out.writeUTF(name);
+                for (Map.Entry<String, ServerInfo> entry : ProxyServer.getInstance().getServers().entrySet()) {
+                    ServerInfo serverInfo = entry.getValue();
+                    serverInfo.ping((result, error) -> {
+                        if (error == null) {
+                            serverInfo.sendData("BungeeCord", b.toByteArray());
+                            new Debugger().sendMessage("[MaSuite] [Warps] Sent delete request");
+                        }
+                    });
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         } else {
             formator.sendMessage(p, "&cAn error occured. Please check console for more details");
         }
