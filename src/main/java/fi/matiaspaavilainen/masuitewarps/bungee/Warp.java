9b5531aa728d8c2c220bdce96838ff1f3c57112c
Masa <masa@simasasolutions.com>
2019-01-01 17:13:07 +0200

    Documented and updated

--- src/main/java/fi/matiaspaavilainen/masuitewarps/bungee/Warp.java
+++ src/main/java/fi/matiaspaavilainen/masuitewarps/bungee/Warp.java
@@ -1,9 +1,8 @@
-package fi.matiaspaavilainen.masuitewarps;
+package fi.matiaspaavilainen.masuitewarps.bungee;
 
-import fi.matiaspaavilainen.masuitecore.MaSuiteCore;
-import fi.matiaspaavilainen.masuitecore.config.Configuration;
-import fi.matiaspaavilainen.masuitewarps.database.Database;
-import fi.matiaspaavilainen.masuitecore.managers.Location;
+import fi.matiaspaavilainen.masuitecore.core.database.ConnectionManager;
+import fi.matiaspaavilainen.masuitecore.core.database.Database;
+import fi.matiaspaavilainen.masuitecore.core.objects.Location;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -14,25 +13,35 @@ import java.util.Set;
 
 public class Warp {
 
-    private Database db = MaSuiteWarps.db;
+    private Database db = ConnectionManager.db;
     private Connection connection = null;
     private PreparedStatement statement = null;
-    private Configuration config = new Configuration();
-    private String tablePrefix = config.load(null, "config.yml").getString("database.table-prefix");
+    private String tablePrefix = db.getTablePrefix();
     // Info
     private int id;
     private String name;
     private String server;
     private Boolean hidden;
     private Boolean global;
-    //Location
 
+    //Location
     private Location location;
 
-
+    /**
+     * An empty constructor for Warp
+     */
     public Warp() {
     }
 
+    /**
+     * Constructor for Warp
+     *
+     * @param name   name of the warp
+     * @param server server of the warp
+     * @param loc    location of the warp
+     * @param hidden is warp hidden or not
+     * @param global is warp global or not
+     */
     public Warp(String name, String server, Location loc, Boolean hidden, Boolean global) {
         this.name = name;
         this.server = server;
@@ -42,33 +51,36 @@ public class Warp {
         this.location = loc;
     }
 
-    public void create(Warp warp) {
+    /**
+     * Create Warp
+     */
+    public void create() {
         String insert = "INSERT INTO " + tablePrefix +
                 "warps (name, server, world, x, y, z, yaw, pitch, hidden, global) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?) " +
                 "ON DUPLICATE KEY UPDATE name = ?, server = ?, world = ?, x = ?, y = ?, z = ?, yaw = ?, pitch = ?, hidden = ?, global = ?;";
         try {
             connection = db.hikari.getConnection();
             statement = connection.prepareStatement(insert);
-            statement.setString(1, warp.getName());
-            statement.setString(2, warp.getServer());
-            statement.setString(3, warp.getLocation().getWorld());
-            statement.setDouble(4, warp.getLocation().getX());
-            statement.setDouble(5, warp.getLocation().getY());
-            statement.setDouble(6, warp.getLocation().getZ());
-            statement.setFloat(7, warp.getLocation().getYaw());
-            statement.setFloat(8, warp.getLocation().getPitch());
-            statement.setBoolean(9, warp.isHidden());
-            statement.setBoolean(10, warp.isGlobal());
-            statement.setString(11, warp.getName());
-            statement.setString(12, warp.getServer());
-            statement.setString(13, warp.getLocation().getWorld());
-            statement.setDouble(14, warp.getLocation().getX());
-            statement.setDouble(15, warp.getLocation().getY());
-            statement.setDouble(16, warp.getLocation().getZ());
-            statement.setFloat(17, warp.getLocation().getYaw());
-            statement.setFloat(18, warp.getLocation().getPitch());
-            statement.setBoolean(19, warp.isHidden());
-            statement.setBoolean(20, warp.isGlobal());
+            statement.setString(1, this.name);
+            statement.setString(2, this.server);
+            statement.setString(3, this.location.getWorld());
+            statement.setDouble(4, this.location.getX());
+            statement.setDouble(5, this.location.getY());
+            statement.setDouble(6, this.location.getZ());
+            statement.setFloat(7, this.location.getYaw());
+            statement.setFloat(8, this.location.getPitch());
+            statement.setBoolean(9, this.hidden);
+            statement.setBoolean(10, this.global);
+            statement.setString(11, this.name);
+            statement.setString(12, this.server);
+            statement.setString(13, this.location.getWorld());
+            statement.setDouble(14, this.location.getX());
+            statement.setDouble(15, this.location.getY());
+            statement.setDouble(16, this.location.getZ());
+            statement.setFloat(17, this.location.getYaw());
+            statement.setFloat(18, this.location.getPitch());
+            statement.setBoolean(19, this.hidden);
+            statement.setBoolean(20, this.global);
             statement.execute();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -80,7 +92,7 @@ public class Warp {
                     e1.printStackTrace();
                 }
             }
-            if(statement != null){
+            if (statement != null) {
                 try {
                     statement.close();
                 } catch (SQLException e) {
@@ -90,6 +102,12 @@ public class Warp {
         }
     }
 
+    /**
+     * Find warp by name
+     *
+     * @param name name of the warp
+     * @return warp with given information
+     */
     public Warp find(String name) {
         Warp warp = new Warp();
         ResultSet rs = null;
@@ -100,7 +118,7 @@ public class Warp {
             statement.setString(1, name);
             rs = statement.executeQuery();
 
-            if(rs == null){
+            if (rs == null) {
                 return new Warp();
             }
             while (rs.next()) {
@@ -141,7 +159,13 @@ public class Warp {
         return warp;
     }
 
-    public Boolean delete(String name){
+    /**
+     * DeleteCommand warp by name
+     *
+     * @param name name of the warp
+     * @return if deleting successful
+     */
+    public Boolean delete(String name) {
         try {
             connection = db.hikari.getConnection();
             statement = connection.prepareStatement("DELETE FROM " + tablePrefix + "warps WHERE name = ?");
@@ -170,7 +194,12 @@ public class Warp {
         return true;
     }
 
-    public Set<Warp> all(){
+    /**
+     * Get all warps from database
+     *
+     * @return SetCommand of warps
+     */
+    public Set<Warp> all() {
         Set<Warp> warps = new HashSet<>();
         ResultSet rs = null;
 
@@ -218,38 +247,65 @@ public class Warp {
         return warps;
     }
 
+    /**
+     * @return id of the warp
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * @param id id of the warp
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * @return name of the warp
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * @param name name of the warp
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * @return server of the warp
+     */
     public String getServer() {
         return server;
     }
 
+    /**
+     * @param server server of the warp
+     */
     public void setServer(String server) {
         this.server = server;
     }
 
+    /**
+     * @return is warp global or not
+     */
     public Boolean isGlobal() {
         return this.global;
     }
 
+    /**
+     * @param global is warp global or not
+     */
     public void setGlobal(Boolean global) {
         this.global = global;
     }
 
+    /**
+     * @return is warp hidden or not
+     */
     public Boolean isHidden() {
         return this.hidden;
     }
@@ -258,10 +314,16 @@ public class Warp {
         this.hidden = hidden;
     }
 
+    /**
+     * @return location of the warp
+     */
     public Location getLocation() {
         return location;
     }
 
+    /**
+     * @param location location of the warp
+     */
     public void setLocation(Location location) {
         this.location = location;
     }
