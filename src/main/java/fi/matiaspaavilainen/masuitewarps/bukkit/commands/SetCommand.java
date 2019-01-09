d33944b5024079c87430b809f8171b0200f13508
Masa <masa@simasasolutions.com>
2019-01-09 15:32:48 +0200

    Added permission for every different warp types + updated to PluginChannel

--- src/main/java/fi/matiaspaavilainen/masuitewarps/bukkit/commands/SetCommand.java
+++ src/main/java/fi/matiaspaavilainen/masuitewarps/bukkit/commands/SetCommand.java
@@ -2,6 +2,7 @@ package fi.matiaspaavilainen.masuitewarps.bukkit.commands;
 
 import fi.matiaspaavilainen.masuitecore.bukkit.chat.Formator;
 import fi.matiaspaavilainen.masuitecore.core.configuration.BukkitConfiguration;
+import fi.matiaspaavilainen.masuitecore.core.objects.PluginChannel;
 import fi.matiaspaavilainen.masuitewarps.bukkit.MaSuiteWarps;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
@@ -9,10 +10,6 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-
-import java.io.ByteArrayOutputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
 
 public class SetCommand implements CommandExecutor {
 
@@ -34,38 +31,33 @@ public class SetCommand implements CommandExecutor {
             if (plugin.checkCooldown(cs, plugin)) return;
 
             Player p = (Player) cs;
-            try (ByteArrayOutputStream b = new ByteArrayOutputStream();
-                 DataOutputStream out = new DataOutputStream(b)) {
-                if (args.length == 1) {
-                    out.writeUTF("SetWarp");
-                    out.writeInt(2);
-                    setWarp(args, p, out);
-                    p.sendPluginMessage(plugin, "BungeeCord", b.toByteArray());
-                } else if (args.length == 2) {
-                    if (args[1].equalsIgnoreCase("hidden") || args[1].equalsIgnoreCase("global")) {
-                        out.writeUTF("SetWarp");
-                        out.writeInt(3);
-                        setWarp(args, p, out);
-                        out.writeUTF(args[1]);
-                        p.sendPluginMessage(plugin, "BungeeCord", b.toByteArray());
+            Location loc = p.getLocation();
+            String l = loc.getWorld().getName() + ":" + loc.getX() + ":" + loc.getY() + ":" + loc.getZ() + ":" + loc.getYaw() + ":" + loc.getPitch();
+            if (args.length == 1) {
+                new PluginChannel(plugin, p, new Object[]{"SetWarp", 2, p.getName(), args[0], l}).send();
+            } else if (args.length == 2) {
+                if (args[1].equalsIgnoreCase("hidden") || args[1].equalsIgnoreCase("global")) {
+                    if (args[1].equalsIgnoreCase("hidden") && !p.hasPermission("masuitewarp.setwarp.hidden")) {
+                        formator.sendMessage(cs, config.load(null, "messages.yml").getString("no-permission"));
+                        return;
                     }
-                } else {
-                    formator.sendMessage((Player) cs, config.load("warps", "syntax.yml").getString("warp.set"));
+                    if (args[1].equalsIgnoreCase("global") && !p.hasPermission("masuitewarp.setwarp.global")) {
+                        formator.sendMessage(cs, config.load(null, "messages.yml").getString("no-permission"));
+                        return;
+                    }
+                    if (!args[1].equalsIgnoreCase("global") && !args[1].equalsIgnoreCase("hidden") && !p.hasPermission("masuitewarp.setwarp.server")) {
+                        formator.sendMessage(cs, config.load(null, "messages.yml").getString("no-permission"));
+                        return;
+                    }
+                    new PluginChannel(plugin, p, new Object[]{"SetWarp", 3, p.getName(), args[0], l, args[1]}).send();
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
+            } else {
+                formator.sendMessage(cs, config.load("warps", "syntax.yml").getString("warp.set"));
             }
-            plugin.in_command.remove(cs);
-
         });
 
+        plugin.in_command.remove(cs);
         return true;
     }
 
-    private void setWarp(String[] args, Player p, DataOutputStream out) throws IOException {
-        out.writeUTF(p.getName());
-        out.writeUTF(args[0]);
-        Location loc = p.getLocation();
-        out.writeUTF(loc.getWorld().getName() + ":" + loc.getX() + ":" + loc.getY() + ":" + loc.getZ() + ":" + loc.getYaw() + ":" + loc.getPitch());
-    }
 }
